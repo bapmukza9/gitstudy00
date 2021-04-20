95751ea5e69ca4c8ef2e74d9281cc22204330dc2
inulee <bapmukza9@gmail.com>
2021-04-20 17:01:13 +0900

    new

--- src/test/main.java
+++ src/test/main.java
@@ -8,12 +8,7 @@ public class main {
 		System.out.println("here is hotfix2");
 
 		System.out.println("good luck!");
-<<<<<<< HEAD
-=======
-		int a = 1;
-		System.out.println(a + "haha");
-
->>>>>>> hotfix2
+		System.out.println("new");
 	}
 
 }
