0b5fb3e671e7416cb525a9a1b9cc213b82ba5ff0
inulee <bapmukza9@gmail.com>
2021-04-20 09:22:45 +0900

    good

--- src/test/main.java
+++ src/test/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("lol");
 		System.out.println("good luck!");
+		int a = 1;
+		System.out.println(a + "haha");
 	}
 
 }
