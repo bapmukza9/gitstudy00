c9c4647dce223742378e179d6577eada912186e8
inulee <bapmukza9@gmail.com>
2021-04-20 19:34:50 +0900

    시작

--- src/test/main.java
+++ src/test/main.java
@@ -3,17 +3,9 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hello world");
-		
-=======
-		System.out.println("lol");
 
-		System.out.println("here is hotfix2");
-
-		System.out.println("good luck!");
-		System.out.println("new");
->>>>>>> 95751ea5e69ca4c8ef2e74d9281cc22204330dc2
 	}
 
 }
