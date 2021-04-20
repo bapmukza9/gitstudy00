a71de6e3e6a2ae673dbd2e13998265f215d3d680
inulee <bapmukza9@gmail.com>
2021-04-20 10:34:52 +0900

    Revert "good"
    This reverts commit 0b5fb3e671e7416cb525a9a1b9cc213b82ba5ff0.

--- src/test/main.java
+++ src/test/main.java
@@ -5,8 +5,6 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("lol");
 		System.out.println("good luck!");
-		int a = 1;
-		System.out.println(a + "haha");
 	}
 
 }
